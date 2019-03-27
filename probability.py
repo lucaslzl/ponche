8f44e49111aeac3b875e8c0e2586f00318dda91e
Lucas <lznladeira@gmail.com>
2019-03-27 14:39:24 -0300

    Command to start simulation

--- probability.py
+++ probability.py
@@ -22,11 +22,22 @@ def main():
 
 	traci.start(sumoCmd)
 
-if __name__ == '__main__':
-	if 'SUMO_HOME' in os.environ:
-		tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-		sys.path.append(tools)
-		import traci
-		main()
-	else:   
-		sys.exit("please declare environment variable 'SUMO_HOME'")
+	step = 0
+	while step < 1000:
+		traci.simulationStep()
+		
+		if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
+			traci.trafficlight.setRedYellowGreenState("0", "GrGr")
+		
+		step += 1
+
+	traci.close()
+
+
+if 'SUMO_HOME' in os.environ:
+	tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+	sys.path.append(tools)
+	import traci
+	main()
+else:
+	sys.exit("please declare environment variable 'SUMO_HOME'")
