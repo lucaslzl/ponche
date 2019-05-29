15200d2164c33b6696f9ab41a3d76b0139d87eed
Lucas <lznladeira@gmail.com>
2019-05-29 09:44:31 -0300

    Plotted results

--- output/plotter.py
+++ output/plotter.py
@@ -50,7 +50,10 @@ class HarryPlotter:
 			if 'duration' in info.attrs.keys() and 'routeLength' in info.attrs.keys() and 'timeLoss' in info.attrs.keys():
 				duration.append(float(info['duration']))
 				route_length.append(float(info['routeLength']))
-				time_loss.append(float(info['timeLoss']))
+				try:
+					time_loss.append(float(info['timeLoss']))
+				except Exception, e:
+					time_loss.append(0.0)
 
 		return {'duration': (np.mean(duration), np.std(duration)),
 				'route_length': (np.mean(route_length), np.std(route_length)),
@@ -138,13 +141,22 @@ class HarryPlotter:
 			json.dump(results, write_file, indent=4)
 
 
-	def separate_mean_std(self, just_to_plot, metric, ordered_keys):
+	def separate_mean_std(self, just_to_plot, metric, keys_order):
 
 		means, stds = [], []
 
-		for key in ordered_keys:
-			means.append(just_to_plot[key][metric][0])
-			stds.append(just_to_plot[key][metric][1])
+		# Austin
+		for key in keys_order:
+			k = [x for x in just_to_plot if key in x and 'austin' in x][0]
+
+			means.append(just_to_plot[k][metric][0])
+			stds.append(just_to_plot[k][metric][1])
+
+		for key in keys_order:
+			k = [x for x in just_to_plot if key in x and 'chicago' in x][0]
+
+			means.append(just_to_plot[k][metric][0])
+			stds.append(just_to_plot[k][metric][1])			
 
 		return means, stds
 
@@ -154,19 +166,22 @@ class HarryPlotter:
 		if not os.path.exists('metric_plots'):
 		    os.makedirs('metric_plots')
 
-		ordered_keys = just_to_plot.keys()
-		ordered_keys.sort()
-
 		plt.clf()
 		ax = plt.subplot(111)
 
-		xlabels = ['Austin #1', 'Austin #2', 'Austin #3', 'Austin #4', 'Austin #5', 'Austin #6', 'Austin #7',
-						'Chicago #1', 'Chicago #2', 'Chicago #3', 'Chicago #4', 'Chicago #5', 'Chicago #6', 'Chicago #7']
+		# xlabels = ['Austin #1', 'Austin #2', 'Austin #3', 'Austin #4', 'Austin #5', 'Austin #6', 'Austin #7',
+		# 				'Chicago #1', 'Chicago #2', 'Chicago #3', 'Chicago #4', 'Chicago #5', 'Chicago #6', 'Chicago #7']
 		colors = ['#800000', '#469990', '#000075', '#e6194B', '#4363d8', '#911eb4', '#aaffc3', '#e6beff']
 
-		means, stds = self.separate_mean_std(just_to_plot, metric, ordered_keys)
+		cities = ['austin', 'chicago']
+		keys_order = ['traffic', 'crimes', 'crashes', 'same', 'mtraffic', 'mcrimes', 'mcrashes']
+
+		xlabels = ['Atraffic', 'Acrimes', 'Acrashes', 'Asame', 'Amtraffic', 'Amcrimes', 'Amcrashes',
+		 				'Ctraffic', 'Ccrimes', 'Ccrashes', 'Csame', 'Cmtraffic', 'Cmcrimes', 'Cmcrashes']
+
+		means, stds = self.separate_mean_std(just_to_plot, metric, keys_order)
 		
-		for index, key in enumerate(ordered_keys):
+		for index, key in enumerate(xlabels):
 			plt.plot(index, means[index], 'o--')
 		#ax.bar(xlabels, means, yerr=stds, align='center', alpha=0.7, color=colors, ecolor='gray', capsize=5)
 		#ax.bar(xlabels, means, align='center', alpha=0.7, color=colors, capsize=5)
@@ -177,18 +192,29 @@ class HarryPlotter:
 		plt.savefig('metric_plots/{0}.pdf'.format(metric), bbox_inches="tight", format='pdf')
 
 
-	def plot(self, results):
+	def filter_keys(self, results, sfilter='context'):
 
-		#print(results.keys())
-		ex_key = results.keys()[0]
-		metrics = results[ex_key].keys()
+		filtered_keys = [x for x in results.keys() if sfilter in x]
+
+		filtered_dict = {}
+		for f in filtered_keys:
+			filtered_dict[f] = results[f]
 		
-		contextual = [x for x in results.keys() if 'context' in x]
-		contextual.sort()
-		print(contextual)
+		metrics = results[filtered_keys[0]].keys()
 
-		#for metric in metrics:
-		#	self.plot_bars(just_to_plot, metric)
+		return filtered_dict, metrics
+
+
+	def plot(self, results):
+		
+		contextual, cmetrics = self.filter_keys(results)
+		mobility, mmetrics = self.filter_keys(results, sfilter='route')
+
+		for metric in cmetrics:
+			self.plot_bars(contextual, metric)
+
+		for metric in mmetrics:
+			self.plot_bars(mobility, metric)
 
 
 if __name__ == '__main__':
@@ -201,4 +227,4 @@ if __name__ == '__main__':
 	hp.read_metric_files(results)
 	hp.save_calculation(results)
 
-	#hp.plot(results)
+	hp.plot(results)
