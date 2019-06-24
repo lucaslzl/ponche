e40f6b837992f96fb82c1b6e962aab99ef7a8846
Lucas Zanco Ladeira <lznladeira@gmail.com>
2019-06-24 17:10:27 -0300

    Remove scipy

--- output/plotter.py
+++ output/plotter.py
@@ -4,7 +4,6 @@ from os import listdir
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-import scipy.stats
 import math
 
 
@@ -47,7 +46,7 @@ class HarryPlotter:
 		a = 1.0 * np.array(data)
 		n = len(a)
 		m, se = np.mean(a), np.std(a)
-		h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+		h = 1.96 * (se/math.sqrt(n))
 		return (m, h)
 
 
