da9166af1d1c598e0df1a61afb9fe17d1e4d8b71
Lucas Zanco Ladeira <lznladeira@gmail.com>
2019-06-24 17:07:13 -0300

    Remove scipy statis sem

--- output/plotter.py
+++ output/plotter.py
@@ -46,7 +46,7 @@ class HarryPlotter:
 	def mean_confidence_interval(self, data, confidence=0.95):
 		a = 1.0 * np.array(data)
 		n = len(a)
-		m, se = np.mean(a), scipy.stats.sem(a)
+		m, se = np.mean(a), np.std(a)
 		h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
 		return (m, h)
 
